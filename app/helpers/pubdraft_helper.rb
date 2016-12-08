0fba6121c5783e5b8462bdf38ef7fd6ced055c6e
Matthew Cowie <matthew@cowie.me>
2016-12-08 17:32:07 -0600

    Update view helpers to read states from class

--- app/helpers/pubdraft_helper.rb
+++ app/helpers/pubdraft_helper.rb
@@ -1,9 +1,9 @@
 module PubdraftHelper
-  def pubdraft_states_for_select
-    [['Published', 'published'], ['Drafted', 'drafted']]
+  def pubdraft_states_for_select(klass)
+    klass.pubdraft_states.map(&:to_select_option)
   end
 
-  def pubdraft_state_options
-    options_for_select(pubdraft_states_for_select)
+  def pubdraft_state_options(klass)
+    options_for_select pubdraft_states_for_select(klass)
   end
 end
